9d377d3a7bc1c6efaaded8e35a7fffd9ba7d7af9
Afia Tabassum <afia.tabassum45@gmail.com>
2025-01-26 15:43:00 +0600

    updating second branch develop

--- 1.py
+++ 1.py
@@ -1,2 +1,10 @@
-for i in range(1, 11):
-    print(i)
+# Number of terms
+n = 10  
+
+# First two terms
+a, b = 0, 1
+
+print("Fibonacci sequence:")
+for _ in range(n):
+    print(a, end=" ")
+    a, b = b, a + b
